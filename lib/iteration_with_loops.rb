14f9adb9d1f928c6946368508674320b62b60d34
Jacinta Calixte <jacinta246@yahoo.com>
2020-06-15 14:08:38 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ results_array= [ ]
 while count < array_of_arrays.length do
 
   inner_count = 0
-  while inner_count < array_of_arrays [count].length do
+  while inner_count < array_of_arrays[count].length do
     results_array << array_of_arrays[count][inner_count]
     inner_count+=1
   end
