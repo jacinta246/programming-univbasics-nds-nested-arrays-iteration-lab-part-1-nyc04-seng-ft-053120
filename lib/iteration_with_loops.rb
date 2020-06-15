3d55fcc12aba5d4bc497deb92091425853cf36f5
Jacinta Calixte <jacinta246@yahoo.com>
2020-06-15 14:41:21 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,6 +2,8 @@ def find_even_values(src)
   row_index = 0
 when array_of_arrays.even?
     element_index = 0
-    while element_index
+    while element_index <src[row_index].count
+      element_index+=1
   end
+  row_index +=1
 end
